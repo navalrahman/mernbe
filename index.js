dd2768f1ae911cbff246ab331d33599b53db83a3
navalrahman <naval.rahman@inetframe.com>
2024-05-30 18:45:41 +0530

    deploy

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const userRoute = require('./routes/userRoute')
 
 const app = express()
 app.use(bodyParser.json())
-// const corseOption = {
-//         origin: ['https://mern-stack-fe-tau.vercel.app'],
-//         // method: ["post","PUT","GET"],
-//       optionsSuccessStatus: 200
-// }
-// app.use(cors(corseOption))
-app.use(cors())
+const corseOption = {
+        origin: ['http://localhost:3000'],
+        method: ["POST","PUT","GET"],
+        optionsSuccessStatus: 200
+}
+app.use(cors(corseOption))
+// app.use(cors())
 dotenv.config()
 app.use(express.json())
 
