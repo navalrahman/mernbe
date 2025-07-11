287c14ed89fcd06b0c566cc9f0aa37c1515d9949
navalrahman <77826561+navalrahman@users.noreply.github.com>
2025-07-11 11:34:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const userRoute = require('./routes/userRoute')
 
 const app = express()
 app.use(bodyParser.json())
-app.use(cors())
+// app.use(cors())
+
+app.use(cors({
+  origin: "https://mern-stack-fe-eta.vercel.app"
+}));
 dotenv.config()
 app.use(express.json())
 
